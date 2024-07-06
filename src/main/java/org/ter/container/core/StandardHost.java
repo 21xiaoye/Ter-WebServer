a75645787a7f3aa98178095ba27796aa02faeb9d
21xiaoye <lao73957459414@163.com>
2024-07-06 17:20:56 +0800

    feat: 实现虚拟主机Host

--- src/main/java/org/ter/container/core/StandardHost.java
+++ src/main/java/org/ter/container/core/StandardHost.java
@@ -1,11 +1,84 @@
 package org.ter.container.core;
 
 import org.ter.container.Container;
+import org.ter.container.Context;
 import org.ter.container.Host;
+import org.ter.exception.LifecycleException;
+import org.ter.lifecycle.LifecycleEvent;
+import org.ter.lifecycle.LifecycleListener;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.WeakHashMap;
 
 public class StandardHost extends ContainerBase implements Host {
+    private final Map<ClassLoader, String> childClassLoader = new WeakHashMap<>();
+    public StandardHost(){
+
+    }
+    @Override
+    public void setName(String name) {
+        if(Objects.isNull(name)){
+            throw new IllegalArgumentException(sm.getString("standardHost.nullName"));
+        }
+        super.setName(name.toLowerCase(Locale.ENGLISH));
+    }
     @Override
     public void addChild(Container container) {
+        if(!(container instanceof Context)){
+            throw new IllegalArgumentException(sm.getString("standardHost.notContext"));
+        }
+        container.addLifecycleListener(new MemoryLeakTrackingListener());
+
         super.addChild(container);
     }
+
+    private class MemoryLeakTrackingListener implements LifecycleListener{
+        @Override
+        public void lifecycleEvent(LifecycleEvent event) {
+            if(event.getType().equals(AFTER_START_EVENT)){
+                if(event.getSource() instanceof Context){
+                    Context context = ((Context) event.getSource());
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void startInternal() throws LifecycleException {
+        System.out.println("启动Host......");
+
+        super.startInternal();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
